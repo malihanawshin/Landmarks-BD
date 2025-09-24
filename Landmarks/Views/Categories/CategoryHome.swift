617fdc3c4543bf746c25368415d1916667d13f14
Maliha Nawshin Rahman <malihanawshin@gmail.com>
2025-09-24 19:46:17 +0200

    Categories and Featured tab

---
+++ Landmarks/Views/Categories/CategoryHome.swift
@@ -0,0 +1,33 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by Maliha on 24/9/25.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    
+    @Environment(ModelData.self) var modelData
+    
+    var body: some View {
+        NavigationSplitView {
+            List{
+                modelData.features[0].image.resizable().scaledToFill().frame(height: 200).clipped().listRowInsets(EdgeInsets())
+                
+                ForEach(modelData.categories.keys.sorted(), id: \.self){
+                    key in CategoryRow(categoryName: key, items: modelData.categories[key]!)
+                }
+            }
+                .navigationTitle("Featured")
+        } detail: {
+            Text("Select a Landmark")
+        }
+    }
+}
+
+#Preview {
+    CategoryHome()
+        .environment(ModelData())
+}
